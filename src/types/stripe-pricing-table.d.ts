aed9631acb86500965d61c7eedcaca88b08b8ca3
disantobusinesses-blip <disantobusinesses@gmail.com>
2025-11-20 14:14:53 +1100

    Add Stripe pricing table embed to pricing page

---
+++ src/types/stripe-pricing-table.d.ts
@@ -0,0 +1,11 @@
+import type { DetailedHTMLProps, HTMLAttributes } from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "stripe-pricing-table": DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
+export {};
