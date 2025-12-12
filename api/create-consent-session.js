d89dbed7da5418237afd4399f3e5754ac59d88ea
disantobusinesses-blip <disantobusinesses@gmail.com>
2025-12-12 17:30:46 +1100

    Update create-consent-session.js

--- api/create-consent-session.js
+++ api/create-consent-session.js
@@ -112,9 +112,9 @@ export default async function handler(req, res) {
 
     // Create Auth Session (Connect Bank)
     const session = await fiskilRequest("/auth/session", {
-      method: "POST",
-      body: JSON.stringify({ end_user_id: fiskilUserId }),
-    });
+  method: "POST",
+  body: JSON.stringify({ end_user: fiskilUserId }),
+});
 
     return res.status(200).json({
       redirect_url: session.redirect_url,
