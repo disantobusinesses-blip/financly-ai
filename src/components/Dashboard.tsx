7155d0dd2b5b8b222cafed27a6fbff143c6a4938
disantobusinesses-blip <disantobusinesses@gmail.com>
2026-01-15 00:47:21 +1100

    mascot.2

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -417,7 +417,7 @@ const Dashboard: React.FC = () => {
         accounts={accounts}
         transactions={transactions}
         region={region}
-        lastUpdated={lastUpdated}
+        lastUpdated={lastUpdated ?? undefined}
       />
     </div>
   );
